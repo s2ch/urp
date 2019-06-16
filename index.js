e94931dd6aa618b77110817eda273fdd1692724c
carmacks2ch <pstbinwebmaster@yandex.ru>
2019-06-17 01:07:56 +0300

    refactor...

--- index.js
+++ index.js
@@ -5,15 +5,16 @@
   const cheerio = require("cheerio");
   const fs      = require("fs");
   const irc     = require("irc");
+  const R       = require("ramda");
 
   const score      = require("./lib/game_score");
   const scoreBoard = score.scoreBoard;
 
-  const gameData = { answer: { index: 0, desc: "" }, answerChoices: {}, photoUrl: {} };
+  const gameData = { checked: { index: 0, title: "" }, titles: [], photoUrl: "" };
   const timerNum = 60; // timer value in seconds
-  const additionalElems = 4; // additional elements in array, right answer plus this amount of additional elements
+  const answers = 5; // set the number of possible answers in this variable
   const arrHelp  = [
-    "Игра «Угадай работу пидора по фото» v. 1.0.1",
+    "Игра «Угадай работу пидора по фото» v. 1.1.0",
     "Идея принадлежит worstie, реализовал carmack",
     "!urp - запустить раунд.",
     "!urp N - выбрать вариант ответа, где N номер предлагаемых вариантов.",
@@ -35,7 +36,7 @@
     sasl: true,
     selfSigned: true,
     certExpired: true,
-    channels: ['#s2ch'],
+    channels: ['#carmackTest'],
     port: 6697,
     autoRejoin: true,
     floodProtection: false,
@@ -47,126 +48,107 @@
 
   // IRC client on
   const client = new irc.Client('chat.freenode.net', 'urp', ircConfig);
-
-  let timer   = {start: 0, stop: 0, timeLeft: 0};
-  let userList = { block: [], winners: [], losers: [] };
-
   const getDateToSec = () => Math.floor(Date.now() / 1000);
   const randMinMax   = (min, max) => Math.floor(Math.random() * (max - min) + min);
   const shuffleArray = arr => arr.map(a => [Math.random(), a]).sort((a, b) => a[0] - b[0]).map(a => a[1]);
 
-  const getRandElemsWoRepeats = (cnt = 0, elems = [], exclude = {}) => {
-    let randIndx, randElems = [], indxCollection = [];
-
-    elems.splice( elems.indexOf(exclude), 1);
-
-    for (let i = 0; i < cnt; i++) {
-      while(true) {
-        randIndx = Math.floor(Math.random() * elems.length);
-        if (indxCollection.indexOf(randIndx) > -1) {
-          continue;
-        }
-
-        if (randElems.indexOf(elems[randIndx] < 0)) {
-          randElems.push( elems[randIndx] );
-          indxCollection.push( randIndx );
-          break;
-        }
-
-      }
-    }
-
-    return randElems;
-  };
-
   const getResumePageData = () => {
-    let headers = {
-      'method': 'GET',
-      'url': `https://www.avito.ru/rossiya/rezume?i=1&p=${randMinMax(1, 100)}`,
-      'headers': {
-        'Host': 'www.avito.ru',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-      }
-    };
-
-    return new Promise((resolve, reject) => {
-      let callback = (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-          let $ = cheerio.load(body);
-          let arrMale = [], arrFemale = [];
-
-          $('.js-catalog_serp .item_resume').each(function () {
-            let male = {}, female = {};
-            let descTitleLink = $(this).find('.item-description-title-link');
-            let descTitleText = $(this).find('.data>p:first-child').text();
-
-            // male
-            if ( descTitleText.match( /муж/i ) ) {
-              male.title  = descTitleLink.attr('title');
-              male.href   = descTitleLink.attr('href');
-              male.gender = 'male';
-
-              arrMale.push(male);
-            }
-
-            // female
-            if ( descTitleText.match( /жен/i ) ) {
-              female.title  = descTitleLink.attr('title');
-              female.href   = descTitleLink.attr('href');
-              female.gender = 'female';
-
-              arrFemale.push(female);
-            }
-          });
-
-          resolve([arrMale, arrFemale]);
+    return new Promise( (resolve, reject) => {
+      let headers = {
+        'method': 'GET',
+        'url': `https://www.avito.ru/rossiya/rezume?i=1&p=${randMinMax(1, 100)}`,
+        'headers': {
+          'Host': 'www.avito.ru',
+          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
         }
       };
 
-      request(headers, callback);
+      let cb = (err, res, body) => {
+        let data = [];
+        let $ = cheerio.load(body);
+
+        $('.js-catalog_serp .item_resume').each(function () {
+          let descTitleLink = $(this).find('.item-description-title-link');
+          let descTitleText = $(this).find('.data>p:first-child').text();
+
+          data.push(
+            {
+              "desc": descTitleText,
+              "title": descTitleLink.attr('title'),
+              "href": descTitleLink.attr('href'),
+              "checked": false,
+              "img": ""
+            }
+          );
+        });
+
+        return R.ifElse(
+          R.propEq(data.length, 0),
+          R.prop(false),
+          R.prop(resolve(data))
+        );
+
+        resolve(data);
+      };
+
+      request(headers, cb);
     });
   };
 
-  const getGameData = () => {
-    let headers = {
-      'method': 'GET',
-      'headers': {
-        'Host': 'www.avito.ru',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
-      }
-    };
+  const getDetailPageData = () => {
+    return new Promise((resolve, reject) => {
+      const randDate = Date.now();
+      const randGender = R.cond([
+        [R.equals(0), R.always("Мужчина")],
+        [R.equals(1), R.always("Женщина")]
+      ]);
 
-    return new Promise(function (resolve, reject) {
+      const isGender = e => e.desc.match(new RegExp(randGender(randDate % 2), "i"));
+
       getResumePageData().then(data => {
-        let randGenderRow = data[randMinMax(0, 2)];
-        let randEntry     = randGenderRow[randMinMax(0, randGenderRow.length - 1)];
-        let arrAnswers    = [randEntry.title];
+        let currentGenderList = shuffleArray(R.filter(isGender, data));
+        let randEntry = R.take(answers, currentGenderList);
+        let pickedAnsw = randEntry[randMinMax(0, answers - 1)];
+        pickedAnsw.checked = true;
 
-        getRandElemsWoRepeats(additionalElems, randGenderRow, randEntry).map((e, i) => arrAnswers.push(e.title));
-        arrAnswers = shuffleArray(arrAnswers);
-        headers.url = `https://www.avito.ru${randEntry.href}`;
-
-        let getAnswerIndx = arrAnswers.indexOf(randEntry.title) + 1;
-
-        gameData.answerChoices = arrAnswers;
-        gameData.answer.index  = getAnswerIndx;
-        gameData.answer.desc   = randEntry.title;
-
-        let callback = (err, res, body) => {
-          let $  = cheerio.load(body), imgUrl;
-          imgUrl = "https:" + $(".gallery-img-frame img").attr("src").replace(/640x480/, '1280x960');
-          gameData.photoUrl = imgUrl;
-          resolve(gameData);
+        let headers = {
+          'method': 'GET',
+          'url': `https://www.avito.ru${pickedAnsw.href}`,
+          'headers': {
+            'Host': 'www.avito.ru',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+          }
         };
 
-        request(headers, callback);
+        let cb = (err, res, body) => {
+          let $  = cheerio.load(body), imgUrl;
+          imgUrl = "https:" + $(".gallery-img-frame img").attr("src").replace(/640x480/, '1280x960');
+          pickedAnsw.img = imgUrl;
+          resolve(randEntry);
+        };
+
+        request(headers, cb);
       });
     });
   };
 
-  // irc message handler
+  const getGameData = () => {
+    return new Promise( (resolve, reject) => {
+      getDetailPageData().then( data => {
+        gameData.checked.index = R.findIndex(R.propEq('checked', true))(data) + 1;
+        gameData.checked.title = R.find(R.propEq('checked', true))(data).title;
+        gameData.photoUrl      = R.find(R.propEq('checked', true))(data).img;
+        gameData.titles        = R.map(e => R.view(R.lensProp("title"), e), data);
+
+        resolve(gameData);
+      });
+    });
+  };
+
+  let timer   = {start: 0, stop: 0, timeLeft: 0};
+  let userList = { block: [], winners: [], losers: [] };
 
   client.addListener('message', function(from, to, message) {
     if (from === "urp") {
@@ -182,15 +164,15 @@
         getGameData().then(response => {
           client.say(to, gameData.photoUrl);
 
-          for (let i = 0, k = 1; i < gameData.answerChoices.length; i++, k++ ) {
-            client.say(to, `${k}). ${gameData.answerChoices[i]}`);
+          for (let i = 0, k = 1; i < answers; i++, k++ ) {
+            client.say(to, `${k}). ${gameData.titles[i]}`);
           }
         });
 
         // Stop
         setTimeout(function () {
           client.say(to, "Раунд завершён, правильный ответ: ");
-          client.say(to, `${gameData.answer.index}). ${gameData.answer.desc}`);
+          client.say(to, `${gameData.checked.index}). ${gameData.checked.title}`);
 
           if (userList.winners.length > 0) {
             client.say(to, `Победители: ${userList.winners.join(', ')}!`);
@@ -231,7 +213,7 @@
         scoreBoard.setNewItem("freenode", "users", from);
         userList.block.push(from);
 
-        if ( parseInt(message.match(/\d/)[0], 10) === gameData.answer.index) {
+        if ( parseInt(message.match(/\d/)[0], 10) === gameData.checked.index) {
           userList.winners.push(from);
           scoreBoard.updateItemPlus("freenode", "users", from);
           scoreBoard.updateItemPlus("freenode", "room", "s2ch");
